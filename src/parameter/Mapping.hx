2e2aacaa3e718dddce28fa2b4ca3e7aa547d8f6d
Mike Almond <mikedotalmond@googlemail.com>
2015-06-18 02:38:48 +0100

    Parameter<T> and Mapping<T>

---
+++ src/parameter/Mapping.hx
@@ -0,0 +1,136 @@
+﻿package parameter;
+
+/**
+ * ...
+ * @author Mike Almond
+ */
+interface IMapping<T> {
+
+	var min:T;
+	var max:T;
+	var range(get,never):T;
+
+	/**
+	 * @param	normalisedValue (0-1)
+	 * @return	A mapped value in the range of min-max
+	 */
+	function map(normalisedValue:T):T;
+
+	/**
+	 * @param	value	A value in the range of min-max
+	 * @return	Normalised value (0-1)
+	 */
+	function mapInverse(value:T):T;
+
+	/**
+	 *
+	 * @return
+	 */
+	function toString():String;
+}
+
+
+@:multiType
+@:forward(min, max, range, map, mapInverse, toString)
+abstract Mapping<T>(IMapping<T>) {
+
+	public function new(minValue:T, maxValue:T);
+
+	@:to static inline function toMapBool<T>(t:IMapping<Bool>, minValue:Bool, maxValue:Bool):MapBool {
+		return new MapBool(minValue, maxValue);
+	}
+
+	@:to static inline function toMapInt<T>(t:IMapping<Int>, minValue:Int, maxValue:Int):MapInt {
+        return new MapInt(minValue, maxValue);
+    }
+
+	@:to static inline function toMapFloat<T>(t:IMapping<Float>, minValue:Float, maxValue:Float):MapFloat {
+        return new MapFloat(minValue, maxValue);
+    }
+}
+
+
+class MapBool implements IMapping<Bool> {
+
+	public var min:Bool;
+	public var max:Bool;
+	public var range(get,never):Bool;
+	inline function get_range() return max != min;
+
+	public function new(min, max) {
+		this.min = min;
+		this.max = max;
+	}
+
+
+	inline public function map(normalizedValue:Bool) return normalizedValue;
+	inline public function mapInverse(value:Bool) return !value;
+
+	inline public function toString() return '[MapBool]';
+}
+
+
+class MapInt implements IMapping<Int> {
+
+	public var min:Int;
+	public var max:Int;
+
+	public var range(get , never):Int;
+	inline function get_range() return max - min;
+
+
+	public function new(min:Int = 0, max:Int = 1 ){
+		this.min = min;
+		this.max = max;
+	}
+
+	inline public function map(normalisedValue:Int)
+		return Math.round(min + normalisedValue * range);
+
+
+	inline public function mapInverse(value:Int)
+		return Math.round((value - min) / range);
+
+
+	inline public function toString()
+		return '[MapInt] min:${min}, max:${max}';
+}
+
+
+class MapFloat implements IMapping<Float> {
+
+	public var min:Float;
+	public var max:Float;
+
+	public var range(get , never):Float;
+	inline function get_range() return max - min;
+
+	public function new(min:Float = 0, max:Float = 1){
+		this.min = min;
+		this.max = max;
+	}
+
+	/**
+	 *
+	 * @param	normalisedValue (0-1)
+	 * @return	A mapped value in the range of min-max
+	 */
+	public inline function map(normalisedValue:Float)
+		return min + normalisedValue * range;
+
+
+	/**
+	 *
+	 * @param	value	A value in the range of min-max
+	 * @return	Normalised value (0-1)
+	 */
+	public inline function mapInverse(value:Float)
+		return ( value - min ) / range;
+
+
+	/**
+	 *
+	 */
+	public inline function toString()
+		return '[MapFloat] min:' + min + ', max:' + max;
+}
---
+++ src/parameter/Parameter.hx
@@ -0,0 +1,151 @@
+﻿package parameter;
+/**
+ * ...
+ * @author Mike Almond
+ */
+
+import hxsignal.Signal;
+import parameter.Mapping;
+
+/**/
+class ParameterBase<T> {
+
+	public var name(default, null):String;
+
+	public var defaultValue(default, null):T;
+	public var normalisedValue(default, null):T;
+	public var normalisedDefaultValue(default, null):T;
+
+	public var mapping(default, null):Mapping<T>;
+	public var change(default, null):Signal<Parameter<T>->Void>;
+
+	private function new(name:String, mapping:Mapping<T>) {
+
+		this.name = name;
+		this.mapping = mapping;
+
+		change = new Signal<Parameter<T>->Void>();
+
+		setDefault(mapping.min);
+	}
+
+	public function setDefault(value:T, normalised:Bool = false) {
+		var normValue:T;
+
+		if (normalised) {
+			normValue = value;
+			value = mapping.map(normValue);
+		} else {
+			normValue = mapping.mapInverse(value);
+		}
+
+		normalisedDefaultValue = normValue;
+		defaultValue = value;
+
+		setValue(normValue, true);
+	}
+
+	public function setValue(value:T, normalised:Bool = false, forced:Bool = false):Void {
+
+		var normValue:T;
+
+		if (normalised) normValue = value;
+		else normValue = mapping.mapInverse(value);
+
+		if (forced || normValue != normalisedValue) {
+			normalisedValue = normValue;
+			change.emit(cast this);
+		}
+	}
+
+	public function setToDefault() {
+		setValue(normalisedDefaultValue, true);
+	}
+
+	public function getValue(normalised:Bool = false):T {
+		if (normalised) return normalisedValue;
+		return mapping.map(normalisedValue);
+	}
+
+	public function invert() {
+		throw 'Error - not implemented';
+	}
+
+	public function addObservers(observers:Array<Parameter<T>->Void>, triggerImmediately = false, once = false) {
+		for (observer in observers) {
+			addObserver(observer, triggerImmediately, once);
+		}
+	}
+
+	public function addObserver(callback:Parameter<T>->Void, triggerImmediately = false, once = false) {
+		if (!change.isConnected(callback)) {
+			change.connect(callback, once ? ConnectionTimes.Once : ConnectionTimes.Forever);
+		}
+		if (triggerImmediately) change.emit(cast this);
+	}
+
+	public function removeObserver(callback:Parameter<T>->Void) {
+		if (change.isConnected(callback)) {
+			change.disconnect(callback);
+		}
+	}
+
+	public function toString():String {
+		return '[Parameter] ${name}, defaultValue:${defaultValue}, mapping:${mapping.toString()}';
+	}
+}
+
+
+class BoolParameter extends ParameterBase<Bool> {
+	public function new(name, mapping:Mapping<Bool>) {
+		super(name, mapping);
+	}
+
+	override public function invert() {
+		setValue(!getValue());
+	}
+}
+
+class IntParameter extends ParameterBase<Int> {
+	public function new(name, mapping:Mapping<Int>) {
+		super(name, mapping);
+	}
+
+	override public function invert() {
+		setValue(-getValue());
+	}
+}
+
+class FloatParameter extends ParameterBase<Float> {
+    public function new(name, mapping:Mapping<Float>) {
+		super(name, mapping);
+	}
+
+	override public function invert() {
+		setValue(-getValue());
+	}
+}
+
+
+@:multiType
+@:forward(name, defaultValue, normalisedValue, normalisedDefaultValue, mapping, change,
+setValue, getValue, setDefault, setToDefault, invert, addObservers, addObserver, removeObserver, toString)
+abstract Parameter<T>(ParameterBase<T>) {
+
+    public function new(name:String, mapping:Mapping<T>);
+
+	@:to static inline function
+		toBoolParameter<T>(t:ParameterBase<Bool>, name:String, mapping:Mapping<Bool>):BoolParameter {
+			return new BoolParameter(name, mapping);
+    }
+
+    @:to static inline function
+		toIntParameter<T>(t:ParameterBase<Int>, name:String, mapping:Mapping<Int>):IntParameter {
+			return new IntParameter(name, mapping);
+    }
+
+	@:to static inline function
+		toFloatParameter<T>(t:ParameterBase<Float>, name:String, mapping:Mapping<Float>):FloatParameter {
+			return new FloatParameter(name, mapping);
+    }
+}
